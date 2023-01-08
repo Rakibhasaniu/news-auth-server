92edde483ce2b4d9951fba8f433b38a0b79dbf05
Rakibhasaniue <rakibhasanoyoniu@gmail.com>
2023-01-08 16:37:47 +0600

    middleware cors implemented

--- index.jsx
+++ index.jsx
@@ -1,10 +1,21 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 
+app.use(cors());
+
+
+
+const categories = require('./data/category.json');
+
 app.get('/', (req, res) => {
-    res.end('News Api Running');
+    res.send('News Api Running');
 });
+
+app.get('/news-categories', (req, res) => {
+    res.send(categories)
+})
 
 app.listen(port, () => {
     console.log('News server running on port', port);
