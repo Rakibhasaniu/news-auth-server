c151a6137940925c0eeb5ce63a31e1d72fb81f83
Rakibhasaniue <rakibhasanoyoniu@gmail.com>
2023-01-13 21:49:46 +0600

    create api for specific id

--- index.jsx
+++ index.jsx
@@ -6,8 +6,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 
-
 const categories = require('./data/category.json');
+const news = require('./data/news.json');
 
 app.get('/', (req, res) => {
     res.send('News Api Running');
@@ -15,6 +15,26 @@ app.get('/', (req, res) => {
 
 app.get('/news-categories', (req, res) => {
     res.send(categories)
+})
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '08') {
+        res.send(news)
+    }
+    else {
+        const categoryNews = news.filter(n => n.category_id === id);
+        res.send(categoryNews);
+    }
+
+})
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    // console.log(id)
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews);
+})
+app.get('/news', (req, res) => {
+    res.send(news);
 })
 
 app.listen(port, () => {
